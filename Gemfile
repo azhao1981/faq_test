79fc3cd0074076073cf921a0fd3e354eeed366b1
azhao1981 <azhao.1981@gmail.com>
2017-06-09 14:24:24 +0800

    云问测试

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://gems.ruby-china.org/"
+
+gem 'rest-client', '1.8.0'
+# gem 'rest-client', '2.0.0'   # 2.0 之后是默认正确的
---
+++ faq.rb
@@ -0,0 +1,25 @@
+require 'rest-client'
+
+res = RestClient.post(
+  'http://test.faqrobot.cn/servlet/ApiChat', 
+  {'access_token' => 'idYRut5yS40rTMXzgrAGqremPcExbORBE290ZTIVDH23Vram2x', 
+  'clientId' => '8ee5ec71-4fca-4be4-b156-797c0c651735', 
+  's' => 'p'}
+)
+
+puts res.request.headers.inspect
+puts res
+
+puts "下面这个是正确的"
+res = RestClient.post(
+  'http://test.faqrobot.cn/servlet/ApiChat', 
+  {
+    'access_token' => 'idYRut5yS40rTMXzgrAGqremPcExbORBE290ZTIVDH23Vram2x', 
+    'clientId' => '8ee5ec71-4fca-4be4-b156-797c0c651735', 
+    's' => 'p'
+  },
+  {
+    accept: :json
+  }
+)
+
